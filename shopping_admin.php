203b2f2a0be8ac55f2484648ff638957ba45184c
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-03 16:18:49 +0900

    first

---
+++ shopping_admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>shopping</title>
+    <link rel="stylesheet" href="/css/master.css">
+    <script type="text/javascript" src="script.js"></script>
+  </head>
+  <body>
+    <header>
+      <span id="logo">shopping</span>
+      <span><a href="#">Iphone</a><a href="#">Galaxy</a><a href="#">LG</a></span>
+    </header>
+  </body>
+</html>
