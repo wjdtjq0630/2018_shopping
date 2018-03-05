c901eb5de5b4ba08c21d679e24416be456cd437a
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-05 20:45:55 +0900

    sql 오류 해결

--- joindb.php
+++ joindb.php
@@ -12,11 +12,10 @@
   $result = mysqli_query($conn, $sql);
   if($result){
     alert('이미 사용중인 아이디 입니다.');
-    locate("./join.php?id=$join_id."); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
+    locate("./join.php?id=$join_id"); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
   }
-  mysqli_row()
-  $sql = "INSERT INTO user (user_name,user_id,user_pw,user_phn,user_adr,user_email)
-       VALUES ('$join_name','$join_id','$user_pw','$join_phn','$join_adr','$user_email')";
+  mysqli_row();
+  $sql = "INSERT INTO user (user_name,user_id,user_pw,user_phn,user_adr,user_email) VALUES ('$join_name','$join_id','$user_pw','$join_phn','$join_adr','$user_email')";
   query($sql);
   locate('./login.php');
  ?>
