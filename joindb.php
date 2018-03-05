9d1540d5ae06d59884043edf5072fbead02763b8
PeterKim <36732516+wjdtjq0630@users.noreply.github.com>
2018-03-05 20:24:14 +0900

    Update joindb.php

--- joindb.php
+++ joindb.php
@@ -12,7 +12,7 @@
   $result = mysqli_query($conn, $sql);
   if($result){
     alert('이미 사용중인 아이디 입니다.');
-    locate("./join.php?id=$join_id."); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
+    locate("./join.php?id=$join_id"); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
   }
   mysqli_row()
   $sql = "INSERT INTO user (user_name,user_id,user_pw,user_phn,user_adr,user_email)
