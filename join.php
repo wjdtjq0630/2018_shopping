22259df531d1070f71dac5df84f4f66f42ae5e0d
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-05 20:22:01 +0900

    아이디 중복시 중복아이디 리턴

--- join.php
+++ join.php
@@ -10,9 +10,16 @@
     <header>
       <form class="" action="joindb.php" method="post">
         <input type="text" name="join_name" placeholder="이름"><br>
-        <input type="text" name="join_id" placeholder="아이디"><br>
-        <input type="text" name="join_pw" placeholder="비밀번호"><br>
-        <input type="text" name="check_pw" placeholder="비밀번호 확인"><br>
+        <?php
+          if(isset($_GET['checked_id'])){ //추후 post방식으로 변경할 것
+            $join_id = htmlspecialchars($_GET['checked_id']);
+            echo '<input type="text" name="join_id" placeholder="아이디" value="'.$join_id.'"><br>';
+          }else{
+            echo '<input type="text" name="join_id" placeholder="아이디"><br>';
+          }
+         ?>
+        <input type="password" name="join_pw" placeholder="비밀번호"><br>
+        <input type="password" name="check_pw" placeholder="비밀번호 확인"><br>
         <input type="text" name="join_phn" placeholder="휴대폰번호"><br>
         <input type="text" name="join_adr" placeholder="주소"><br>
         <input type="text" name="join_email" placeholder="이메일"><br>
--- joindb.php
+++ joindb.php
@@ -11,7 +11,8 @@
   $sql = "SELECT * FROM user WHERE user_id='$join_id'";
   $result = mysqli_query($conn, $sql);
   if($result){
-    alert_back('이미 사용중인 아이디 입니다.');
+    alert('이미 사용중인 아이디 입니다.');
+    locate("./join.php?id=$join_id."); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
   }
   mysqli_row()
   $sql = "INSERT INTO user (user_name,user_id,user_pw,user_phn,user_adr,user_email)
