91968f3fd4360152865de9a68dfd6643016a7bf5
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-06 10:09:39 +0900

    mysqli_row()실행 실패시 alert

--- func_sql.php
+++ func_sql.php
@@ -11,9 +11,12 @@
   }
   function mysqli_row($sql,$array_name){ //입력한 sql문 실행 후 결과를 배열에 넣음
     $result = mysqli_query($conn, $sql);
+    if(!$result){ //실행 결과가 없을 경우 경고창 출력 후 뒤로가기
+      alert_back('error');
+    }
     $array_name = mysqli_fetch_array($result);
   }
-  function array_row_num($sql, $num){ //sql문 실행 후 변수에 행 개수를 넣음 
+  function array_row_num($sql, $num){ //sql문 실행 후 변수에 행 개수를 넣음
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
     $num = mysqli_num_rows($row);
