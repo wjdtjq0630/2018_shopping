c2a845b450e6f891c96540d7a314ccb4803e3e6a
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-07 21:18:04 +0900

    alert_url()추가, check_admin/log 수정

--- func_alert.php
+++ func_alert.php
@@ -4,6 +4,9 @@
   function alert($alert){ //경고창 출력
     echo "<script>alert(".$alert.");</script>";
   }
+  function alert_url($alert,$url){
+    echo "<script>alert(".$alert."); location.href='".$url."';</script>";
+  }
   function alert_back($alert){ //경고창 출력 후 url 이동
     echo "<script>alert('".$alert."'); history.back();</script>";
   }
@@ -12,14 +15,14 @@
   }
   function check_log(){ //이미 로그인한 경우 메인 페이지로 이동
     if(isset($_SESSION['user_name'])){
-      alert('이미 로그인 하셨습니다.');
-      locate('./index.php');
-    }
-  function check_admin(){ //관리자가 아닐 경우 돌려보냄
-    if($_SESSION['user_name'] != "admin"){
-      alert('관리자 전용 페이지 입니다.');
-      locate('./index.php');
+      alert_url('이미 로그인 하셨습니다.','./index.php');
     }
   }
+  function check_admin(){ //로그인하지 않았거나 관리자가 아닐 경우 돌려보냄
+      if(empty($_SESSION['user_id'])){
+        alert_url('로그인 후 이용해주세요','./login.php');
+    } else if($_SESSION['user_id'] != "admin"){
+        alert_url('관리자 전용 페이지 입니다.','./index.php');
+    }
   }
  ?>
