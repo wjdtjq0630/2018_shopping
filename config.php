195772641a32a06ab85e8042a541fa6523f01946
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-11 20:54:13 +0900

    set charset in mysql

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
   include 'func_alert.php';
   include 'func_sql.php';
-
+  
   $db_host = "localhost";
   $db_user = "phoneshop";
   $db_password = "asdf1234";
@@ -9,8 +9,12 @@
 
   $conn = mysqli_connect($db_host,$db_user,$db_password,$db_name);
 
+  mysqli_query($conn, "set session character_set_connection=utf8;");
+  mysqli_query($conn, "set session character_set_results=utf8;");
+  mysqli_query($conn, "set session character_set_client=utf8;");
+
   if(mysqli_connect_errno($conn)){
     echo "데이터베이스 연결 실패:".mysqli_connect_error();
   }
 
- ?>
+  ?>
