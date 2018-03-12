ae668e2136e6c351913d66f79525acb0fa256145
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-12 16:50:04 +0900

    옵션 추가 팝업창

---
+++ add_option.php
@@ -0,0 +1,15 @@
+<?php
+  include 'config.php';
+  session_start();
+  //check_admin();
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>옵션 추가하기</title>
+  </head>
+  <body>
+    팝업창입니다.
+  </body>
+</html>
