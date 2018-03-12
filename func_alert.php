02578e127622b12ba96abf6abc3defc00e81f441
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-12 16:20:31 +0900

    따옴표 오류 해결

--- func_alert.php
+++ func_alert.php
@@ -2,10 +2,10 @@
   //경고창, url 이동을 위한 함수
 
   function alert($alert){ //경고창 출력
-    echo "<script>alert(".$alert.");</script>";
+    echo "<script>alert('".$alert."');</script>";
   }
   function alert_url($alert,$url){
-    echo "<script>alert(".$alert."); location.href='".$url."';</script>";
+    echo "<script>alert('".$alert."'); location.href='".$url."';</script>";
   }
   function alert_back($alert){ //경고창 출력 후 url 이동
     echo "<script>alert('".$alert."'); history.back();</script>";
