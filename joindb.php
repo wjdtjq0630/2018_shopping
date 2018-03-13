45a5d05c1f555ee7012d306f5b99855c27ed4730
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-13 19:33:44 +0900

    아이디 중복 시 코드 변경

--- joindb.php
+++ joindb.php
@@ -11,20 +11,14 @@
   $join_adr = mysqli_real_escape_string($conn,$_POST['join_adr']);
   $join_email = mysqli_real_escape_string($conn,$_POST['join_email']);
 
-  if($join_pw == $check_pw){
-    $sql = "SELECT * FROM user WHERE user_id='$join_id'";
-
-    echo $join_name.'<br>'.$join_id.'<br>'.$join_pw.'<br>'.$join_phn.'<br>'.$join_adr.'<br>'.$join_email;
-    // if($result = mysqli_query($conn, $sql)){
-    //   alert('이미 사용중인 아이디 입니다.');
-    //   locate("./join.php?checked_id=$join_id"); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
-    // } else{
-    //   $sql = "INSERT INTO user_info (user_id,user_pw,user_phn,user_adr,user_email,user_name) VALUES ('$join_id','$user_pw','$join_phn','$join_adr','$user_email','$join_name')";
-    //   //query($sql);
-    //   mysqli_query($conn, $sql);
-    //   //locate('./login.php');
-    // }
+  $sql = "SELECT * FROM user_info WHERE user_id='$join_id'";
+  if(!$result = mysqli_query($conn, $sql)){
+    $sql = "INSERT INTO user_info (user_id,user_pw,user_phn,user_adr,user_email,user_name) VALUES ('$join_id','$user_pw','$join_phn','$join_adr','$user_email','$join_name')";
+    query($sql);
+    locate('./login.php');
   } else{
-    alert_back('비밀번호가 일치하지 않습니다.');
+    alert_back('이미 사용중인 아이디 입니다.');
+    //history.back();해주면 결과 값 같이 넘어감
+    //locate("./join.php?checked_id=$join_id"); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
   }
  ?>
