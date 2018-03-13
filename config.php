e0b55d046efc0aa7448a8f122738c8f6b9d31906
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-13 19:36:30 +0900

    include func 코드 위치 변경

--- config.php
+++ config.php
@@ -1,6 +1,4 @@
 <?php
-  include 'func_alert.php';
-  include 'func_sql.php';
 
   $db_host = "localhost";
   $db_user = "phoneshop";
@@ -8,12 +6,15 @@
   $db_name = "phoneshop";
 
   $conn = mysqli_connect($db_host,$db_user,$db_password,$db_name);
+  
+  include 'func_alert.php';
+  include 'func_sql.php';
 
   mysqli_query($conn, "set session character_set_connection=utf8;");
   mysqli_query($conn, "set session character_set_results=utf8;");
   mysqli_query($conn, "set session character_set_client=utf8;");
   header('Content-Type: text/html; charset=utf-8');
-  
+
   if(mysqli_connect_errno($conn)){
     echo "데이터베이스 연결 실패:".mysqli_connect_error();
   }
