05e835a2944af22d3e8f105204a5ca0966d92562
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-13 11:11:38 +0900

    query 오류 해결 및 비밀번호 확인 하기

--- joindb.php
+++ joindb.php
@@ -3,21 +3,28 @@
   session_start();
   check_log();
 
-  $join_name = mysql_real_escape_string($_POST['join_name'],$conn);
-  $join_id = mysql_real_escape_string($_POST['join_id'],$conn);
-  $join_pw = mysql_real_escape_string($_POST['join_pw'],$conn);
-  $join_phn = mysql_real_escape_string($_POST['join_phn'],$conn);
-  $join_adr = mysql_real_escape_string($_POST['join_adr'],$conn);
-  $join_email = mysql_real_escape_string($_POST['join_email'],$conn);
+  $join_name = mysqli_real_escape_string($conn,$_POST['join_name']);
+  $join_id = mysqli_real_escape_string($conn,$_POST['join_id']);
+  $join_pw = mysqli_real_escape_string($conn,$_POST['join_pw']);
+  $check_pw = $_POST['check_pw'];
+  $join_phn = mysqli_real_escape_string($conn,$_POST['join_phn']);
+  $join_adr = mysqli_real_escape_string($conn,$_POST['join_adr']);
+  $join_email = mysqli_real_escape_string($conn,$_POST['join_email']);
 
-  $sql = "SELECT * FROM user WHERE user_id='$join_id'";
+  if($join_pw == $check_pw){
+    $sql = "SELECT * FROM user WHERE user_id='$join_id'";
 
-  if($result = mysqli_query($conn, $sql)){
-    alert('이미 사용중인 아이디 입니다.');
-    locate("./join.php?checked_id=$join_id"); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
+    echo $join_name.'<br>'.$join_id.'<br>'.$join_pw.'<br>'.$join_phn.'<br>'.$join_adr.'<br>'.$join_email;
+    // if($result = mysqli_query($conn, $sql)){
+    //   alert('이미 사용중인 아이디 입니다.');
+    //   locate("./join.php?checked_id=$join_id"); //아이디 중복시 중복인 아이디 되돌려주기 //추후 post방식으로 변경할 것
+    // } else{
+    //   $sql = "INSERT INTO user_info (user_id,user_pw,user_phn,user_adr,user_email,user_name) VALUES ('$join_id','$user_pw','$join_phn','$join_adr','$user_email','$join_name')";
+    //   //query($sql);
+    //   mysqli_query($conn, $sql);
+    //   //locate('./login.php');
+    // }
   } else{
-    $sql = "INSERT INTO user (user_name,user_id,user_pw,user_phn,user_adr,user_email) VALUES ('$join_name','$join_id','$user_pw','$join_phn','$join_adr','$user_email')";
-    query($sql);
-    locate('./login.php');
+    alert_back('비밀번호가 일치하지 않습니다.');
   }
  ?>
