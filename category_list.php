ea69ecd2c9b81a838d143a4fd5b114f9483de3a7
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-15 19:00:22 +0900

    카테고리 추가 및 카테고리 링크 목록

---
+++ category_list.php
@@ -0,0 +1,26 @@
+<?php
+  include 'config.php';
+  session_start();
+  check_admin();
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>카테고리 관리</title>
+   </head>
+   <body>
+     <form class="" action="add_category.php" method="post"> <!--새로운 카테고리를 등록-->
+       새로운 카테고리:<br><input type="text" name="new_category"><br>
+       <input type="submit" value="등록하기">
+     </form>
+         <?php //카테고리로 이동
+            $sql = "SELECT * FROM category";
+            if($result = mysqli_query($conn, $sql)){
+                while($row = mysqli_fetch_array($result)){
+                  echo "<a href='./category.php?name={$row['name']}'>{$row['id']}:{$row['name']}</a><br>";
+                }
+            }
+          ?>
+   </body>
+ </html>
