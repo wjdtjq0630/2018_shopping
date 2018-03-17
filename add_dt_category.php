88406d6bf4a898dbcea1dff248e0225c72731fdd
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-17 18:13:00 +0900

    중복 카테고리 확인 오류 해결

--- add_dt_category.php
+++ add_dt_category.php
@@ -4,14 +4,14 @@
   check_admin();
 
   $id = $_POST['category_id'];
-  $new_dt_category = mysqli_real_escape_string($conn, $_POST['$new_dt_category']);
+  $new_dt_category = mysqli_real_escape_string($conn, $_POST['new_dt_category']);
 
   $sql = "SELECT * FROM dt_category WHERE id='$id' AND name='$new_dt_category'";
-  if($result = mysqli_query($conn, $sql)){
+  if(mysqli_fetch_array(mysqli_query($conn, $sql))){
     alert_back('이미 존재하는 카테고리 입니다.');
   } else{
-      $sql = "INSERT INTO dt_category (id,name) VALUES ('$id','$name')";
+      $sql = "INSERT INTO dt_category (id,name) VALUES ('$id','$new_dt_category')";
       query($sql);
-      locate("./category.php?id='$id'");
+      locate("./category.php?id=$id");
     }
 ?>
