af08d4d5e622c5ab9a8959bd321013b3bdf50207
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-17 18:20:27 +0900

    타이틀 내용,카테고리id값,리스트 출력 형식 수정

--- category.php
+++ category.php
@@ -3,25 +3,33 @@
   session_start();
   check_admin();
 
+  if(empty($_GET['id'])){
+    alert_back('잘못된 접근입니다.');
+  }
+
   $id = htmlspecialchars($_GET['id']);
+  $sql = "SELECT * FROM category WHERE id='$id'";
+  $result = mysqli_query($conn, $sql);
+  $category = mysqli_fetch_array($result);
+  $name = $category['name'];
  ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>카테고리 - <?php echo $name;?></title>
+    <title>카테고리 <?php echo $id.'-'.$name;?></title>
   </head>
   <body>
       <form class="" action="add_dt_category.php" method="post"> <!--새로운 세부 카테고리 등록 -->
-        새로운 세부 카테고리:<input type="text" name="new_dt_category"><br>
-        <input type="hidden" name="category_id" value="<?php echo $id ?>">
+        새로운 세부 카테고리:<input type="text" name="new_dt_category">
+        <input type="hidden" name="category_id" value="<?php echo $id;?>">
         <input type="submit" value="등록하기">
       </form>
       <?php
         $sql = "SELECT * FROM dt_category WHERE id='$id'";
         if($result = mysqli_query($conn, $sql)){
           while($row = mysqli_fetch_array($result)){
-            echo "<a href='./dt_category.php?id={$row['id']}'>{$row['id']}: {$row['name']}</a>";
+            echo "<a href='./dt_category.php?id={$row['id']}'>{$row['id']}-{$row['name']}</a><br>";
           }
         }
        ?>
