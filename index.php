cd0253d2abbc4f9ff53b5a3d61e6406040a3b98e
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-18 14:24:21 +0900

    마이페이지 링크 추가

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
   if(isset($_SESSION['user_id']) && $_SESSION['user_name']){ //로그인 한 경우
     $user_id = htmlspecialchars($_SESSION['user_id']);
     $user_name = htmlspecialchars($_SESSION['user_name']);
-    echo "{$user_name}({$user_id})님 환영합니다.<a href='./logout.php'>로그아웃</a>";
+    echo "{$user_name}({$user_id})님 환영합니다.<a href='./logout.php'>로그아웃</a><br>";
+    echo "<a href='./mypage.php'>마이페이지</a>";
     if($user_id == "admin"){
-      echo '<br><a href="./admin.php">관리자 페이지</a>';
+      echo ' <a href="./admin.php">관리자 페이지</a>';
     }
   } else{ //로그인 하지 않은 경우 로그인 링크 출력
     echo '<a href="./login.php">로그인</a> <a href="./join.php">회원가입</a>';
