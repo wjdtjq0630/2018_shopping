d4bf2308e4ba0268052621b52154123d719a3c19
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-18 14:24:56 +0900

    add check_unlog()

--- func_alert.php
+++ func_alert.php
@@ -14,8 +14,13 @@
     echo "<script>location.href='".$url."';</script>";
   }
   function check_log(){ //이미 로그인한 경우 메인 페이지로 이동
-    if(isset($_SESSION['user_name'])){
+    if(isset($_SESSION['user_name']) || isset($_SESSION['user_id'])){
       alert_url('이미 로그인 하셨습니다.','./index.php');
+    }
+  }
+  function check_unlog(){ //로그인 하지 않은 경우 로그인 페이지로 이동
+    if(empty($_SESSION['user_name']) || empty($_SESSION['user_id'])){
+      alert_url('로그인 후 이용하세요.','./login.php');
     }
   }
   function check_admin(){ //로그인하지 않았거나 관리자가 아닐 경우 돌려보냄
