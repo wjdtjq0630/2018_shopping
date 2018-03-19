ba521964943cddb7f49c1cb9ccf0d036b3707704
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-19 10:19:08 +0900

    mysqli_row(),array_num_rows() 주석처리

--- func_sql.php
+++ func_sql.php
@@ -10,19 +10,19 @@
       alert('success');
     }
   }
-  function mysqli_row($sql,$array_name){ //입력한 sql문 실행 후 결과를 배열에 넣음
-    global $conn;
-    $result = mysqli_query($conn, $sql);
-    if(!$result){ //실행 결과가 없을 경우 경고창 출력 후 뒤로가기
-      alert_back('error');
-    } else{
-        $array_name = mysqli_fetch_array($result);
-  }
-  }
-  function array_row_num($sql, $num){ //sql문 실행 후 변수에 행 개수를 넣음
-    global $conn;
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_array($result);
-    $num = mysqli_num_rows($row);
-  }
+  //사용 불가 오류 발생
+  // function mysqli_row($sql){ //입력한 sql문 실행 후 결과를 배열에 넣음
+  //   global $conn;
+  //   $result = mysqli_query($conn, $sql);
+  //   if(!$result){ //실행 결과가 없을 경우 경고창 출력 후 뒤로가기
+  //     alert_back('error');
+  //   } else{
+  //       return mysqli_fetch_array($result);
+  // }
+  // }
+  // function array_num_rows($sql){ //sql문 실행 후 변수에 행 개수를 넣음
+  //   global $conn;
+  //   $result = query($sql);
+  //   return mysqli_fetch_array($result);
+  // }
  ?>
