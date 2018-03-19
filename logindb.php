aa8cc18711f7a633ee2ffde0cd91161b57271ae1
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-20 00:15:16 +0900

    관리자 로그인의 경우 세션에 관리자 정보 추가

--- logindb.php
+++ logindb.php
@@ -7,14 +7,22 @@
   $login_pw = mysqli_real_escape_string($conn,$_POST['login_pw']);
 
   $sql = "SELECT * FROM user_info WHERE user_id='$login_id'";
-  $result = mysqli_query($conn, $sql); //입력한 아이디에 해당하는 회원정보가 존재하는가?
-  if(!$user = mysqli_fetch_array($result)){ //아이디가 존재하지 않을 경우 경고창 출력 후 뒤로가기
-    alert_back('존재하지 않는 아이디 입니다!');
-  } else if($user['user_pw'] == $login_pw){ //저장한 회원정보와 비밀번호가 일치할 경우
-        $_SESSION['user_id'] = $user['user_id']; //세션에 회원정보 저장
-        $_SESSION['user_name'] = $user['user_name'];
-        alert_url('로그인 성공','./index.php'); //알림창 출력 후 메인 페이지로 이동
-    } else{ //비밀번호가 일치하지 않을 경우
-        alert_back('비밀번호가 일치하지 않습니다.'); //알림창 출력 후 뒤로가기
+  if($row = mysqli_fetch_array(mysqli_query($conn,$sql))){
+    if($row['user_pw'] == $login_pw){
+      $_SESSON['user_id'] = $row['user_id'];
+      $_SESSON['user_name'] = $row['user_name'];
+      if($row['admin'] == 1){
+        $_SESSION['admin_id'] = $row['user_id'];
+        $_SESSION['admin_name'] = $row['user_name'];
+        alert_url('관리자 로그인 성공!','./admin.php');
+      }else{
+        alert_url('로그인 성공!','./index.php');
       }
+    } else{
+        alert_back('비밀번호가 일치하지 않습니다!');
+      }
+  } else{
+     alert_back('존재하지 않는 아이디 입니다!');
+    }
+
  ?>
