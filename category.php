1b5de83bd865acb4ec98e777ec67fed7bc995094
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-19 10:17:36 +0900

    카테고리명 변경 추가

--- category.php
+++ category.php
@@ -17,15 +17,13 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>카테고리 <?php echo $id.'-'.$name;?></title>
+    <title>카테고리 <?php echo $id.':'.$name;?></title>
   </head>
   <body>
-    <h3><?php echo "{$id}-{$name}" ?></h3>
-      <form class="" action="add_dt_category.php" method="post"> <!--새로운 세부 카테고리 등록 -->
-        새로운 세부 카테고리:<input type="text" name="new_dt_category">
-        <input type="hidden" name="category_id" value="<?php echo $id;?>">
-        <input type="submit" value="등록하기"><a href="./admin.php">관리자 메인페이지</a>
-      </form>
+    <h3><?php echo "{$id}:{$name}" ?></h3>
+    <form class="" action="edit_category_name.php" method="post">
+      카테고리 이름:<input type="text" name="category_name" value="<?php echo $name;?>"><input type="submit" value="변경하기">
+    </form>
       <?php
         $sql = "SELECT * FROM dt_category WHERE id='$id'";
         if($result = mysqli_query($conn, $sql)){
@@ -34,5 +32,11 @@
           }
         }
        ?>
+       <form class="" action="add_dt_category.php" method="post"> <!--새로운 세부 카테고리 등록 -->
+         새로운 세부 카테고리:<input type="text" name="new_dt_category">
+         <input type="hidden" name="category_id" value="<?php echo $id;?>">
+         <input type="submit" value="등록하기">
+       </form>
+       <input type="button" value="관리자 메인페이지" onclick="location.href='./admin.php'">
   </body>
 </html>
