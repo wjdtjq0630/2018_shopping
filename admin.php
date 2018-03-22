9220a7770ce8e30c317112606c87da25a4014acb
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-22 20:59:51 +0900

    상품등록페이지 링크 수정

--- admin.php
+++ admin.php
@@ -13,7 +13,7 @@
      <ul>
        <h3>관리자 페이지</h3>
        <li><a href="./category_list.php">카테고리 관리</a></li>
-       <li><a href="./productform.php">상품 등록</a></li>
+       <li><a href="./register_product_form.php">상품 등록</a></li>
        <li><a href="./edit_product.php">상품 수정</a></li>
        <input type="button" value="메인 페이지" onclick="location.href='./index.php'">
      </ul>
