3856cd985437febb236463737568a15050ac31e5
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-24 14:13:20 +0900

    이미지 파일 업로드 실패 오류 해결 할 것

--- register_product_db.php
+++ register_product_db.php
@@ -9,24 +9,61 @@
   $price = mysqli_real_escape_string($conn, $_POST['p_price']);
   $delivery = mysqli_real_escape_string($conn, $_POST['p_delivery']);
   $return = mysqli_real_escape_string($conn, $_POST['exchange']);
-  $a_s = mysqli_real_escape_string($conn, $_POST['a/s']);
+  $a_s = mysqli_real_escape_string($conn, $_POST['a_s']);
   //option list
 
   if(empty($name)||empty($model)||empty($price)||empty($delivery)||empty($return)||empty($a_s)){
     alert_back("필수 항목을 모두 입력하세요!");
-  }else{
-    if(empty($_FILES['p_image']['name'])){
-      alert_back("대표 이미지가 입력되지 않았습니다!");
-    } else if($_FILES['p_image']['name']>255){
-      alert_back("대표 이미지 파일명이 너무 깁니다!");
-    }
+    exit;
   }
-  if(empty($_FILES['p_detailimg']['name'])){
+
+  if(empty($_FILES['p_img']['name'])){
+    alert_back("대표 이미지가 입력되지 않았습니다!");
+    exit;
+  } else if(strlen($_FILES['p_img']['name'])>255){
+    alert_back("대표 이미지 파일명이 너무 깁니다!");
+    exit;
+  }
+
+  if(empty($_FILES['p_detail_img']['name'])){
     alert_back("상세정보 이미지가 입력되지 않았습니다!");
-  } else if($_FILES['p_detailimg']['name']>255){
+    exit;
+  } else if(strlen($_FILES['p_detail_img']['name'])>255){
     alert_back("상세정보 이미지 파일명이 너무 깁니다!");
+    exit;
   }
-  $sql = "INSERT INTO product (name,model,price,img,dt_img,delivery,exchange,a_s) VALUES ('$name','$model','$price','$img','$dt_img','$delivery','$return','$a_s')";
+
+  $date = date("Y-m-d H:i:s",time());
+  $img_dir = "./product_img/";
+  $img_hash = md5($date.$_FILES['p_img']['name']);
+  $img_upfile = $img_dir.$img_hash;
+  if(is_uploaded_file($_FILES['p_img']['temp_name'])){
+    if(!move_uploaded_file($_FILES['p_img']['temp_name'],$img_upfile)){
+      alert_back("대표 이미지 이동 에러");
+      exit;
+    }
+  } else{
+    alert_back("대표 이미지 업로드 에러");
+    exit;
+  }
+
+  $detail_dir = "./product_detail/";
+  $detail_hash = md5($date.$_FILES['p_detail_img']['name']);
+  $dt_upfile = $detail_dir.$detail_hash;
+  if(is_uploaded_file($_FILES['p_detail_img']['temp_name'])){
+    if(!move_uploaded_file($_FILES['p_detail_img']['temp_name'],$dt_upfile)){
+      alert_back("상세 정보 이미지 이동 에러");
+      exit;
+    }
+  } else{
+      alert_back("상세 정보 이미지 업로드 에러");
+      exit;
+  }
+
+  $sql = "INSERT INTO product (name,model,price,delivery,exchange,a_s) VALUES ('$name','$model','$price','$delivery','$return','$a_s')";
   query($sql);
-  alert_url("상품 등록 성공","./dt_category.php?id={$id}&dt_id={$dt_id}");
+  $sql = "INSERT INTO product_img (dir,dt_dir) VALUES ('$img_upfile','$dt_upfile')";
+  query($sql);
+  //alert_url("상품 등록 성공","./dt_category.php?id={$id}&dt_id={$dt_id}");
+
  ?>
