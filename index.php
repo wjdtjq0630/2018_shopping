f3baae50818b079a1a723b58fb032b5ebb43d8d1
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-25 23:51:36 +0900

    마이페이지,로그아웃 링크 클래스 지정

--- index.php
+++ index.php
@@ -46,9 +46,9 @@
           if(isset($_SESSION['user_id']) && isset($_SESSION['user_name'])){ //로그인 한 경우
             $user_id = htmlspecialchars($_SESSION['user_id']);
             $user_name = htmlspecialchars($_SESSION['user_name']);
-            echo "<li><a href='./mypage.php'>{$user_name}({$user_id})</a></li><li><a href='./logout.php'>로그아웃</a></li>";
+            echo "<li class='right'><a href='./logout.php'>로그아웃</a></li><li class='right'><a href='./mypage.php'>{$user_name}({$user_id})</a></li>";
             if($user_id == "admin"){
-              echo '<li><a href="./admin.php">관리자 페이지</a></li>';
+              echo '<li class="right"><a href="./admin.php">관리자 페이지</a></li>';
             }
           } else{ //로그인 하지 않은 경우 로그인 링크 출력
             echo '<li class="right"><a href="./join.php">회원가입</a></li>
