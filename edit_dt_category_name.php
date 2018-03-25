de358b888eb483a278f123b26f1f6d6f657ae844
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-25 19:54:34 +0900

    세부 카티고리명 변경(중복시 변경 불가)

---
+++ edit_dt_category_name.php
@@ -0,0 +1,18 @@
+<?php
+  include 'config.php';
+  session_start();
+  check_admin();
+
+  $category_id = mysqli_real_escape_string($conn, $_POST['category_id']);
+  $dt_id = mysqli_real_escape_string($conn, $_POST['dt_category_name']);
+  $new_name = mysqli_real_escape_string($conn, $_POST['new_name']);
+
+  $sql = "SELECT * FROM dt_category WHERE id='$category_id' AND name='$new_name'";
+  if(!mysqli_num_rows(mysqli_query($conn, $sql))){
+    $sql = "UPDATE dt_category SET name='$new_name' WHERE id='$category_id' AND dt_id='$dt_id'";
+    query($sql);
+    locate("./dt_category.php?id={$category_id}&dt_id={$dt_id}");
+  } else{
+    alert_back('카테고리에 같은 이름의 세부 카테고리가 이미 존재합니다!');
+  }
+ ?>
