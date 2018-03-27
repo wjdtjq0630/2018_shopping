d186d6e572b22d5fe618facc055932ace2bf260b
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-27 21:28:05 +0900

    카테고리명 변경시 공백 제한

--- edit_category_name.php
+++ edit_category_name.php
@@ -5,8 +5,14 @@
 
   $category_id = mysqli_real_escape_string($conn, $_POST['category_id']);
   $new_name = mysqli_real_escape_string($conn, $_POST['category_name']);
+
+  if(empty($new_name)){
+    alert_back("카테고리명을 입력하세요");
+    exit;
+  }
+  
   $sql = "UPDATE category SET name='$new_name' WHERE id='$category_id'";
   query($sql);
   locate("./category.php?id={$category_id}");
-  
+
  ?>
